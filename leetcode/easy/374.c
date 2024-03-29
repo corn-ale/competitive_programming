fc4813f88cc515840ac2b1b065f2bdada8e84bbc
Kornél Konyári <konyarikornel@gmail.com>
2024-03-30 00:32:08 +0100

    374

--- leetcode/easy/374.c
+++ leetcode/easy/374.c
@@ -4,7 +4,7 @@
 // @return 	    -1 if num is higher than the picked number
 //   			1 if num is lower than the picked number
 //   			otherwise return 0
-//int guess(int num);
+int guess(int num);
 
 
 int guessNumber(int n)
