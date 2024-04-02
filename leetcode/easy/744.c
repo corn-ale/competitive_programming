fa7db27d7319eb1c352d9c12dfef112720b75c84
Kornél Konyári <konyarikornel@gmail.com>
2024-04-03 00:40:22 +0200

    744.c

---
+++ leetcode/easy/744.c
@@ -0,0 +1,10 @@
+#include <string.h>
+
+char nextGreatestLetter(char* letters, int lettersSize, char target) {
+	for (int i = 0; i < lettersSize ; i++)
+	{
+		if (letters[i] > target)
+			return letters[i];
+	}
+	return (letters[0]);
+}
